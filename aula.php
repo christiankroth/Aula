79380c266d8c9d476c577d3e470d64d66babedd6
christiankroth <christiankroth@hotmail.com>
2017-06-26 21:11:54 -0300

    Adicionado arquivo aula.php

---
+++ aula.php
@@ -0,0 +1,16 @@
+<?php
+define('PIS', 0.65);
+define('COFINS', 3);
+define('CSLL', 1);
+
+$valorFinal = calcula_total(150, 15);
+
+function calcula_total($totalVendido, $frete){
+	$totalVendido = $totalVendido + ($totalVendido*(PIS/100));
+	$totalVendido = $totalVendido + ($totalVendido*(COFINS/100));
+	$totalVendido = $totalVendido + ($totalVendido*(CSLL/100));
+	
+	return $totalVendido;
+}
+
+?>
